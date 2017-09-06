616fcab233f8ec694bdacf1d4f82f2acff3a7078
wfang33 <wenxinfang97@gmail.com>
2017-09-06 19:04:16 -0400

    window sum

---
+++ Amazon/windowSum.java
@@ -0,0 +1,36 @@
+package amazon;
+
+import java.util.*;
+
+public class windowSum {
+
+	public static List<Integer> _windowSum(List<Integer> input, int k){
+		
+		List<Integer> res = new ArrayList<>();
+		if(input == null || input.size() == 0 || k <= 0) return res;
+		int sum = 0;
+		
+		for(int i = 0; i <= k - 1 && i < input.size(); i++)
+			sum += input.get(i);
+		
+		res.add(sum);
+		
+		int slow = 0, fast = k;
+		while(fast < input.size()){
+			sum -= input.get(slow++);
+			sum += input.get(fast++);
+			res.add(sum);
+		}
+		
+		return res;
+	}
+	
+	public static void main(String[] args){
+		List<Integer> input = new ArrayList<>();
+		input.addAll(Arrays.asList(2,3,4,2,5,7,8,9,6));
+		List<Integer> output = _windowSum(input, 5);
+		for(int i: output)
+			System.out.println(i + " ");
+	}
+	
+}
