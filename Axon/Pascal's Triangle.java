d7d91bd18b05ba3273dcf374254e8b36d181d7ef
wfang33 <wenxinfang97@gmail.com>
2017-10-28 16:12:23 -0400

    triangle

---
+++ Axon/Pascal's Triangle.java
@@ -0,0 +1,18 @@
+// O(N^2)
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+    	List<List<Integer>> res = new LinkedList<>();
+    	if(numRows <= 0) return res;
+    	for(int i = 0; i < numRows; i++){
+    		List<Integer> list = new LinkedList<>();
+    		list.add(1);
+    		if(i >= 1){
+    			for(int j = 0; j < res.get(i - 1).size() - 1; j++)
+    				list.add(res.get(i - 1).get(j) + res.get(i - 1).get(j + 1));
+    			list.add(1);
+    		}
+    		res.add(list);
+    	}
+    	return res;
+    }
+}
