3b0ad3029568cd578259c4dd14d004fd3f229bf2
wfang33 <wenxinfang97@gmail.com>
2017-11-29 17:19:38 -0500

    Medium

--- Leetcode/348. ticTacToe.java
+++ Leetcode/348. ticTacToe.java
@@ -1,3 +1,42 @@
+// O(1)
+class TicTacToe {
+    private int n;
+    private int[] rows;
+    private int[] cols;
+    private int dial1 = 0;
+    private int dial2 = 0;
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        this.rows = new int[n];
+        this.cols = new int[n];
+        this.n = n;
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        int toAdd = player == 1 ? 1 : -1;
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        if(row == col)
+            dial1 += toAdd;
+        if(row + col == n - 1)
+            dial2 += toAdd;
+        if(Math.abs(rows[row]) == n || Math.abs(cols[col]) == n || Math.abs(dial1) == n || Math.abs(dial2) == n)
+            return player;
+        return 0;
+    }
+
+}
+
+
+// O(n)
 class TicTacToe {
     private int[][] board;
     private int n;
@@ -62,9 +101,3 @@ class TicTacToe {
         return false;
     }
 }
-
-/**
- * Your TicTacToe object will be instantiated and called as such:
- * TicTacToe obj = new TicTacToe(n);
- * int param_1 = obj.move(row,col,player);
- */
